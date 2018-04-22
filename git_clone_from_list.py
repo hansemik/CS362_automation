a56f5effb9f7986f8da08414dc8369e69203d1c1
Mikkel Hansen <hansemik@flip3.engr.oregonstate.edu>
2018-04-22 13:20:17 -0700

    Added make functionality

--- git_clone_from_list.py
+++ git_clone_from_list.py
@@ -6,11 +6,30 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process name of input text file.')
 parser.add_argument('file',help='text file containing onid in column one and \ngithub usernames in column two')
+parser.add_argument('-assignment_num',help='integer of assignment e.g. 2')
 args = parser.parse_args()
 
 userlist = args.file
 
+if args.assignment_num == None:
+   assignment_num = 'default'
+else:
+   assignment_num = str(args.assignment_num)
+
 with open(userlist) as f:
+
+   os.chdir('../')
+   time.sleep(1)
+
+   if not os.path.exists('Grading'):
+      os.makedirs('Grading')
+   os.chdir('Grading')
+
+   if not os.path.exists(''.join(['assignment',assignment_num])):
+      os.makedirs(''.join(['assignment',assignment_num]))
+   os.chdir(''.join(['assignment',assignment_num]))
+
+
    for line in f:
       onid,github_username = line.strip().split(",")
       if github_username != '':
